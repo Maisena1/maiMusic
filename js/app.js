cf67f6ff2d23d0c869ee1d06fe93a1d314c3db28
Jeremias Mai <jeremiasmaiasd@gmail.com>
2023-12-06 17:57:08 -0300

    three commi

--- js/app.js
+++ js/app.js
@@ -1,21 +1,32 @@
 const msjalert = document.querySelector(".container_msj_alert");
+const btnmsjalert = document.querySelector(".alert__btn");
 const containerall = document.querySelector(".container");
+const container = document.querySelector(".list-music__music");
 const pop_ups = document.querySelector(".pop-ups");
 const btnadd = document.querySelectorAll(".btn-add");
 const btnburger = document.querySelector("#burgerlist_mobile");
 const list_Music = document.querySelector(".music-music");
-const template = document.querySelector(".list-music__music");
+const template = document.querySelector(".template_music");
 // le asigno el atributo hidden a la lista de musica para no mostrarla todavia
-const music_list = [];
-list_Music.setAttribute("hidden", "true");
-pop_ups.setAttribute("hidden", "true");
 let state = false;
-document.addEventListener("click", (e) => {
-  if (e.target.matches(".alert__btn")) {
-    containerall.style.filter = "none";
-    msjalert.style.display = "none";
+const music_list = [];
+
+btnburger.addEventListener("click", (e) => {
+  if (state) {
+    pop_ups.classList.add("showburger");
+    pop_ups.classList.remove("hideburger");
+    state = false;
+  } else {
+    pop_ups.classList.add("hideburger");
+    pop_ups.classList.remove("showburger");
+    state = true;
   }
 });
+
+btnmsjalert.addEventListener("click", (e) => {
+  console.log(e);
+});
+
 const addmusic = (e) => {
   if (music_list.length === 7) {
     return console.log("ya esta lleno");
@@ -24,6 +35,7 @@ const addmusic = (e) => {
   }
   showmusic(e);
 };
+
 const showmusic = (e) => {};
 btnadd.forEach((item) => {
   item.addEventListener("click", addmusic);
